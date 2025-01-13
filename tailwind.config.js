15cc3fd0ec9d6cec6d565447776e201be8a5c7d2
busraayverdi <busraayverdi.95@gmail.com>
2025-01-13 23:30:00 +0300

    packagejson changed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./app/**/*.{js,jsx}"],
+  important: true,
+  content: ["./app/**/*.{html,js,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans]
+      },
+      keyframes: {
+        shimer: {
+          "100%": { transform: "translateX(100%)" },
+        },
+      },
+      animation: {
+        shimer: "shimmer 1.5s infinite",
+      }
+    },
   },
   plugins: [],
 };
